64d23911b2cd0e2904e9ff42ce502b1bca882ee7
Nivethitha Kumar <Nivi@Nivethithas-MacBook-Air.local>
2015-07-03 01:10:37 -0700

    Initial Commit

---
+++ MyVigil/MyVigil/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyVigil
+//
+//  Created by Nivethitha Kumar on 7/3/15.
+//  Copyright (c) 2015 Nivethitha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
